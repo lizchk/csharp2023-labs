80bc115d92ba95a118a52086074a4cc0e1576086
lizchk <107196142+lizchk@users.noreply.github.com>
2023-03-04 16:58:18 +0200

    View is added

---
+++ 01lab_Yakovenko/Views/ZodiacView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using _01lab_Yakovenko.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01lab_Yakovenko.Views
+{
+    
+    public partial class ZodiacView : UserControl
+    {
+        private ZodiacViewModel _viewModel;
+        public ZodiacView()
+        {
+
+            InitializeComponent();
+            DataContext = _viewModel = new ZodiacViewModel();
+        }
+    }
+}
