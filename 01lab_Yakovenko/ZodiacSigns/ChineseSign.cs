2ba41a74ae7b6955de654e44597724d77180d056
lizchk <107196142+lizchk@users.noreply.github.com>
2023-03-04 16:59:16 +0200

    Tools are added

---
+++ 01lab_Yakovenko/ZodiacSigns/ChineseSign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01lab_Yakovenko.ZodiacSigns
+{
+    public enum ChineseSign
+    {
+        Rat,
+        Ox,
+        Tiger,
+        Rabbit,
+        Dragon,
+        Snake,
+        Horse,
+        Goat,
+        Monkey,
+        Rooster,
+        Dog,
+        Pig
+    }
+}
---
+++ 01lab_Yakovenko/ZodiacSigns/ZodiacSign.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01lab_Yakovenko.ZodiacSigns
+{
+    public enum ZodiacSign
+    {
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn,
+        Aquarius,
+        Pisces
+    }
+}
